80f4b55038879ed38bc0dec5e56516e17fa682d3
Tetteh Israel <Tettehisrael1@gmail.com>
2022-07-30 18:36:45 +0100

    variables.tf created

---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+#variables.tf
+
+variable "domain_name" {
+  type = string
+  description = "The domain name for the website."
+}
+
+variable "bucket_name" {
+  type = string
+  description = "The name of the bucket without the www. prefix. Normally domain_name."
+}
+
+
